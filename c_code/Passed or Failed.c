877e1118101d3f437348b7ad48edc087ddb81069
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-27 03:19:22 +0600

    Add files via upload

---
+++ c_code/Passed or Failed.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int mark;
+
+    printf("Enter the number:");
+    scanf("%d",&mark);
+
+    if(mark>=33){
+            printf("Passed");
+        }
+    else {
+            printf("Failed");
+        }
+
+
+ return 0;
+}
