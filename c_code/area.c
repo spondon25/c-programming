4dc23502d59579748c6e56eb3f493acffc0f2ef2
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-27 16:15:45 +0600

    Add files via upload

---
+++ c_code/area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float b,h,area;
+    printf("Enter the base:");
+    scanf("%f",&b);
+    printf("Enter the height:");
+    scanf("%f",&h);
+    area=(b*h)/2;
+    printf("the area of tringle is=% 2f\n",area);
+    getch();
+
+}
