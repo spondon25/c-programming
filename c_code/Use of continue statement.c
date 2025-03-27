2ec72e48e0c013224c1dc8abe2c9ea09e1ee98e8
Asif Faisal Spondon <spondon171@gmail.com>
2025-03-27 15:53:30 +0600

    Add files via upload

---
+++ c_code/Use of continue statement.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    float number, sum=0;
+
+    for(i=1;i<=10;i++){
+        printf("Enter n%d:",i);
+        scanf("%f",&number);
+
+        if(number<0){
+            continue;
+        }
+        sum+=number;
+    }
+
+    printf("Sum is %f",sum);
+
+    return 0;
+}
+
+
